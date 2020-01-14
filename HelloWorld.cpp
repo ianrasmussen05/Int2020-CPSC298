a1efe5fb934bbed011a371b37988d2ae50fb539c
root <irasmussen@chapman.edu>
2020-01-14 20:45:12 +0000

    This is my first assignment for interterm

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+  Ian Rasmussen
+  2317200
+  irasmussen@chapman.edu
+  CPSC 298-01
+  Hello World Assignment
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+
+  return 0;
+}
