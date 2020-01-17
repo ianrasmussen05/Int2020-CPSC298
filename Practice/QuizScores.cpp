c0d95bc4cd1c0d804a4ab4380e300cfe2e57c1af
ianrasmussen05 <irasmussen@chapman.edu>
2020-01-17 01:20:46 +0000

    Updated Quiz Scores file

--- Practice/QuizScores.cpp
+++ Practice/QuizScores.cpp
@@ -5,33 +5,17 @@ using namespace std;
 int main (int argc, char **argv)
 {
   int quizArray[5];
-  int quiz1 = 0;
-  int quiz2 = 0;
-  int quiz3 = 0;
-  int quiz4 = 0;
-  int quiz5 = 0;
+  int quiz = 0;
   int totalQuiz = 0;
   double avg = 0.0;
 
-  cout << "Enter the grade from the first quiz: ";
-  cin >> quiz1;
-  quizArray[0] = quiz1;
+  for (int i = 0; i < 5; ++i)
+  {
+    cout << "Enter the score of the quiz: ";
+    cin >> quiz;
 
-  cout << "Enter the grade from the second quiz: ";
-  cin >> quiz2;
-  quizArray[1] = quiz2;
-
-  cout << "Enter the grade from the third quiz: ";
-  cin >> quiz3;
-  quizArray[2] = quiz3;
-
-  cout << "Enter the grade from the fourth quiz: ";
-  cin >> quiz4;
-  quizArray[3] = quiz4;
-
-  cout << "Enter the grade from the fifth quiz: ";
-  cin >> quiz5;
-  quizArray[4] = quiz5;
+    quizArray[i] = quiz;
+  }
 
   for (int i = 0; i < 5; ++i)
   {
