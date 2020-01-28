0e7d18f295cb4ee3acb6ada4b73f6bef986ad4aa
ianrasmussen05 <irasmussen@chapman.edu>
2020-01-28 21:05:39 +0000

    Pointer Practice

--- Practice/Class_Demo/main.cpp
+++ Practice/Class_Demo/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv)
 {
+  /*
   // On the stack, quicker and easier to use. Run into obstacles using a lot of code
   CPSC298Utils cp;
   cout << "ON THE STACK: There are " << cp.countVowels("Ian") << " vowels." << endl;
@@ -12,6 +13,19 @@ int main(int argc, char **argv)
 
   // must use delete word to delete the new instance of cps so there isn't memory leaks
   delete cps;
+  */
+
+  int myVar = 5;
+  int *myPtr;
+  myPtr = &myVar;
+
+  cout << "myVar: " << myVar << endl;
+  cout << "myPtr: " << myPtr << endl;
+  cout << "Dereference myPtr: " << *myPtr << endl;
+
+  // Dereference and changes the value to 8
+  *myPtr = 8;
+  cout << "myVar after update: " << myVar << endl;
 
   return 0;
 }
