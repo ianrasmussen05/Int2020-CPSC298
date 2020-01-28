08b57ac84b6d4190043d5cd284e06cf9fc09084c
ianrasmussen05 <irasmussen@chapman.edu>
2020-01-28 20:57:23 +0000

    Updated Assignment 2, still not finished

--- Assignment_2/conversion.cpp
+++ Assignment_2/conversion.cpp
@@ -1,31 +1,63 @@
+/*
+  Ian Rasmussen
+  2317200
+  irasmussen@chapman.edu
+  CPSC 298-01
+  Assignment 2
+  conversion
+*/
+
 #include <iostream>
 
 using namespace std;
 
-string userInput()
+void userInput(int& feet, int& inches)
 {
+  cout << "Enter the length in feet: ";
+  cin >> feet;
 
+  cout << "Enter the remaining length in inches: ";
+  cin >> inches;
 
-  return "\0";
+  return;
 }
 
-double convertFeetToMeters(int feet, int inches)
+void convertFeetToMeters(int feet, int inches, int& meters, int& centimeters)
 {
+  int total = 0;
+  double totalFeet = 0;
+  double meterToFeet = 0;
+  double totalMeter = 0;
+  double centimeter = 0;
 
+  meters = 1.0 / 0.3048;
+  totalFeet = feet + (inches / 12) + ((inches % 12) / 12.0);
+  totalMeter = totalFeet / meter;
 
-  return 0;
+  total = totalMeter;
+
+  return;
 }
 
-string outputValues()
+void outputValues(int feet, int inches, int meters, int centimeters)
 {
+  cout << "With " << feet << " feet and " << inches << " inches. " << endl;
+  cout << "It converts to " << meters << " meters and " << centimeters << " centimeters." << endl;
 
-
-  return "\0";
+  return;
 }
 
 int main(int argc, char **argv)
 {
+  int feet = 0;
+  int inches = 0;
+  int meters = 0;
+  int centimeters = 0;
 
+  userInput(feet, inches);
+
+
+  outputValues(feet, inches, meters, centimeters);
 
   return 0;
 }
--- Assignment_2/zeroboth.cpp
+++ Assignment_2/zeroboth.cpp
@@ -11,9 +11,27 @@
 
 using namespace std;
 
-// The void function where it sets the two parameters to zero 
-void bothZero(int num1 = 0, int num2 = 0)
+// The void function where it sets the two parameters to zero
+void bothZero(int& num1, int& num2)
 {
-  cout << "Number 1: " << num1 << endl;
-  cout << "Number 2: " << num2 << endl;
+  num1 = 0;
+  num2 = 0;
+}
+
+int main(int argc, char **argv)
+{
+  int firstNum = 10;
+  int secondNum = 5;
+
+  cout << "The starting numbers: " << endl;
+  cout << firstNum << endl;
+  cout << secondNum << endl;
+
+  bothZero(firstNum, secondNum);
+
+  cout << "Now to zero:" << endl;
+  cout << firstNum << endl;
+  cout << secondNum << endl;
+
+  return 0;
 }
