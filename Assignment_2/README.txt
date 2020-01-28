def8c3e8694b4801c2b32733f703fb036d0d197d
ianrasmussen05 <irasmussen@chapman.edu>
2020-01-28 23:41:48 +0000

    Finished version of Assignment 2

--- Assignment_2/README.txt
+++ Assignment_2/README.txt
@@ -5,3 +5,9 @@ CPSC 298-01
 Assignment 2
 
 Source Files: intarray.cpp, 2darray.cpp, zeroboth.cpp, salestax.cpp, conversion.cpp
+
+The limitations I had on this assignment was figuring out how to use addresses or '&'.
+Everything runs perfectly, just found the conversion problem challenging.
+
+I did reference a classmate, Frank, to get a better understanding in conversion.
+I used a similar method of getting the user input.
--- Assignment_2/conversion.cpp
+++ Assignment_2/conversion.cpp
@@ -11,35 +11,38 @@
 
 using namespace std;
 
+// This method gets the user input when called on. The two parameters use an address
+// to allow the variables to be changed in another method if needed.
 void userInput(int& feet, int& inches)
 {
+  // Asked the user to input the feet
   cout << "Enter the length in feet: ";
   cin >> feet;
 
+  // Asked the user to input the inches
   cout << "Enter the remaining length in inches: ";
   cin >> inches;
 
   return;
 }
 
-void convertFeetToMeters(int feet, int inches, int& meters, int& centimeters)
+// This function is the conversion from feet and inches to meters and centimeters.
+// I used addresses on meters and centimeters to allow the variables to be changed
+// inside another method.
+void convertFeetToMeters(int feet, int inches, float& meters, float& centimeters)
 {
-  int total = 0;
-  double totalFeet = 0;
-  double meterToFeet = 0;
-  double totalMeter = 0;
-  double centimeter = 0;
+  float feetToMeter = 0.3048; // This is how many meters is in one foot
+  float inchToMeter = 0.0254; // This is how many meters is in one inch
 
-  meters = 1.0 / 0.3048;
-  totalFeet = feet + (inches / 12) + ((inches % 12) / 12.0);
-  totalMeter = totalFeet / meter;
-
-  total = totalMeter;
+  // Found the conversion by multiplying given feet to meters and given inches with meters
+  meters = (feet * feetToMeter) + (inches * inchToMeter);
+  centimeters = (feet * (feetToMeter * 100)) + (inches * (inchToMeter * 100));
 
   return;
 }
 
-void outputValues(int feet, int inches, int meters, int centimeters)
+// This is the printing method for the program. Inputs four parameters and prints each value.
+void outputValues(int feet, int inches, float meters, float centimeters)
 {
   cout << "With " << feet << " feet and " << inches << " inches. " << endl;
   cout << "It converts to " << meters << " meters and " << centimeters << " centimeters." << endl;
@@ -49,15 +52,31 @@ void outputValues(int feet, int inches, int meters, int centimeters)
 
 int main(int argc, char **argv)
 {
+  // Declare all the variables to zero or null
   int feet = 0;
   int inches = 0;
-  int meters = 0;
-  int centimeters = 0;
+  float meters = 0;
+  float centimeters = 0;
+  string stringInput = "\0";
 
-  userInput(feet, inches);
+  while (true)
+  {
+    userInput(feet, inches); // Calls onto the userInput method and changes the feet and inches values
+    convertFeetToMeters(feet, inches, meters, centimeters); // Calls the conversion method and changes meters and centimeters
+    outputValues(feet, inches, meters, centimeters); // Calls the output method and prints the values
 
-
-  outputValues(feet, inches, meters, centimeters);
+    // This block is asking the user if they want to run the program again.
+    cout << "Enter 'stop' to exit the program... ";
+    cin >> stringInput;
+    if (stringInput == "stop")
+    {
+      break;
+    }
+    else
+    {
+      continue;
+    }
+  }
 
   return 0;
 }
